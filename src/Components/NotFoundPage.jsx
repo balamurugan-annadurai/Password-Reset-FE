40437bfc7ef7c6e1c0bcebb3c2e52ea62f22e64e
balamurugan-annadurai <abalamurugan2003@gmail.com>
2024-07-25 21:08:38 +0530

    Code update

--- src/Components/NotFoundPage.jsx
+++ src/Components/NotFoundPage.jsx
@@ -16,6 +16,8 @@ const NotFoundPage = () => {
                 count == 1 ?
                     <div>
                         <h3 className='text-center'>Link has expired</h3>
+                        <p>or</p>
+                        <h3 className='text-center'>Network issues please refresh your screen</h3>
                         <img className='not-found-page-img' src={img} alt="" />
                     </div> :
 
